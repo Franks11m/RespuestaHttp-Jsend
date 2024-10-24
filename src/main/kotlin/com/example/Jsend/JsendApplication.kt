9399725b7b40079c94d5ef946ac153a9afc4846a
Franks11m <146012181+Franks11m@users.noreply.github.com>
2024-10-24 17:40:27 -0500

    first commit

---
+++ src/main/kotlin/com/example/Jsend/JsendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Jsend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JsendApplication
+
+fun main(args: Array<String>) {
+	runApplication<JsendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/Jsend/JsendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Jsend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JsendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
